0179612e0a226043e75c1495b515e4008ebc15a8
Eduardo32 <p.eduardo.32@gmail.com>
2021-06-02 16:15:48 -0300

    Implementações novas Model, Repositorie e Servise para os appointments

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-console */
 import express from 'express';
-import router from './routes';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-app.use(router);
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server satarted on port 3333');
