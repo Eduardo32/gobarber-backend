ec5ff792362c695ddf3b8662703ba844b4b7932a
Eduardo32 <p.eduardo.32@gmail.com>
2021-08-28 14:39:05 -0300

    Reestruturando e ajustando importações

---
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+/* eslint-disable class-methods-use-this */
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '@shared/errors/AppError';
+import Appointment from '../infra/typeorm/entities/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmetnDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmetnDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new AppError('This appointment is already booked');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider_id,
+      date: appointmetnDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
